f6a133e7bd0bc525e55af4558243597eca95fbf7
Brent Nichols <brentnic@gmail.com>
2015-10-01 22:17:50 -0400

    add controllers to configured states

--- app/config.js
+++ app/config.js
@@ -11,15 +11,18 @@
       $stateProvider
         .state('index', {
           url: '/',
-          templateUrl: 'dashboard/dashboard.html'
+          templateUrl: 'dashboard/dashboard.html',
+          controller: 'DashboardController as dashboardController'
         })
         .state('user', {
           url: '/user',
-          templateUrl: 'user/user.html'
+          templateUrl: 'user/user.html',
+          controller: 'UserController as userController'
         })
         .state('widget', {
           url: '/widget',
-          templateUrl: 'widget/widget.html'
+          templateUrl: 'widget/widget.html',
+          controller: 'WidgetController as widgetController'
         });
 
       // If route isn't matched
