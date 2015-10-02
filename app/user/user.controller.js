4f87db57048db7ce8a9fbfde18251595e991966c
Brent Nichols <brentnic@gmail.com>
2015-10-01 23:19:57 -0400

    hard coded data binding of users

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -10,6 +10,8 @@
 		.controller('UserController', UserController);
 
 	function UserController() {
-
+		var user = this;
+		user.users = [{"id":"1","name":"Brent Nichols","avatarURL":"https://s.gravatar.com/avatar/92f8cd3318f64c07a367a76c0599dc12?s=48"},
+			{"id":"2","name":"JBoss McHuston","avatarURL":"https://s.gravatar.com/avatar/e11550b1bf793d43639292b196374262?s=48"}];
 	}
 })();
