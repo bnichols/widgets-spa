d283fc58b2c0d1177b0976ebf6a0e7cb3324d10b
Brent Nichols <brentnic@gmail.com>
2015-10-01 23:28:13 -0400

    hard coded data bindings for widgets

--- app/widget/widget.controller.js
+++ app/widget/widget.controller.js
@@ -8,6 +8,7 @@
 		.controller('WidgetController', WidgetController);
 
 	function WidgetController() {
-
+		var widget = this;
+		widget.widgets = [{"id":"1","name":"bar","color":"blue","price":"$99.50","melts":"no","inventory":"888"}];
 	}
 })();
