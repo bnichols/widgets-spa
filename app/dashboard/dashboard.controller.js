afe6e1fa77b3e28049ab753270d5c7394bc56772
Brent Nichols <brentnic@gmail.com>
2015-10-01 22:59:37 -0400

    hardcoded data bindings for dashboard

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -10,6 +10,8 @@
 		.controller('DashboardController', DashboardController);
 
 	function DashboardController() {
-
+		var dash = this;
+		dash.widgets = [{"id":"1","name":"Widget A"},{"id":"2","name":"Widget B"}];
+		dash.users = [{"id":"1","name":"Joe Bloggs"},{"id":"2","name":"Brent Nichols"},{"id":"3","name":"Thomas Hopkins"}];
 	}
 })();
